f721db429548380c5259bb62a5797d415041df5d
michael-kestler <michael.s.kestler@gmail.com>
2023-06-16 16:55:16 -0400

    added props and data to pull from home and work on this weekend

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,55 @@
-
+<template>
+<section>
+    <header><h1>Users</h1></header>
+    <new-user @add-user="addUser"></new-user>
+    <ul>
+        <active-user>
+            v-for="user in users"
+            :key = "id"
+            :id = "user.id"
+            :userName: "user.userName"
+            :age="user.age"
+        </active-user>
+    </ul>
+</section>
+</template>
 
 <script>
+import ActiveUser from './components/ActiveUser.vue';
     export default{
+  components: { ActiveUser },
+        data(){
+            return{
+                users: [
+                    {
+                        id: '001',
+                        userName: 'jsmith22',
+                        age: '25'
+                    },
+                    {
+                        id: '002',
+                        userName:'astanwyck',
+                        age: '33'
+                    }
+                ],
+            };
+        },
+
+        methods: {
+            addUser(userName, age){
+                const newUser = {
+                    id: new Date().toISOString,
+                    userName: userName,
+                    age: age,
+                }
+                 this.users.push(newUser)
+            }
+        }
+
+       
 
     }
+
+    
 
 </script>
--- src/components/ActiveUser.vue
+++ src/components/ActiveUser.vue
@@ -1,23 +1,44 @@
 <template>
-    <header><h2>Username</h2></header>
-    <h3>Age</h3>
+<section>
+    <h2>UserName: {{userName}}</h2>
+    <h3>Age: {{age}}</h3>
+</section>
 </template>
 
 <script>
 export default{
     props: {
+        id:{
+            type: String,
+            required: true
+        },
         userName: {
             type: String,
             required: true
         },
         age: {
-            type: Number,
+            type: String,
             required: true
         }
+    },
+
+    data(){
+        return{
+                users: [
+                    {
+                        id: '001',
+                        userName: 'jsmith22',
+                        age: '25'
+                    },
+                    {
+                        id: '002',
+                        userName:'astanwyck',
+                        age: '33'
+                    }
+                ],
+            };
     }
 
 }
-    
-
 
 </script>
