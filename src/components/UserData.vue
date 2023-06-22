173795cb61449225f4cdb763d5a29eedf154cf16
michael-kestler <michael.s.kestler@gmail.com>
2023-06-22 10:15:49 -0400

    added template and inputs for the user data component

---
+++ src/components/UserData.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        <input type="text" placeholder="Name"/>
+        <input type="text" placeholder="Age"/>
+
+    </section>
+</template>
+
+<script>
+    export default{}
+</script>
